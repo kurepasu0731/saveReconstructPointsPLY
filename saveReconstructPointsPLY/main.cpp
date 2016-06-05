839cae13396b33cca26f0723cec553ea83048685
kurepasu0731 <kurepasu0731@gmail.com>
2016-06-06 00:29:08 +0900

    mesh生成実装

--- saveReconstructPointsPLY/main.cpp
+++ saveReconstructPointsPLY/main.cpp
@@ -6,7 +6,8 @@
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/filters/voxel_grid.h>
-
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/surface/gp3.h>
 
 //XMLファイル読み込み
 std::vector<cv::Point3f> loadXMLfile(const std::string &fileName);
@@ -14,13 +15,14 @@ std::vector<cv::Point3f> loadXMLfile(const std::string &fileName);
 //PLY形式で保存
 void savePLY(std::vector<cv::Point3f> points, const std::string &fileName);
 void savePLY_with_normal(std::vector<cv::Point3f> points, std::vector<cv::Point3f> normals, const std::string &fileName);
+void savePLY_with_normal_mesh(std::vector<cv::Point3f> points, std::vector<cv::Point3f> normals, std::vector<cv::Point3i> meshes, const std::string &fileName);
 
-//TODO: 法線を求める（PCL）
 //法線ベクトルを求める
 std::vector<cv::Point3f> getNormalVectors(std::vector<cv::Point3f> points);
-
 //ダウンサンプリング
 std::vector<cv::Point3f> getDownSampledPoints(std::vector<cv::Point3f> points, float size);
+//三角メッシュを生成
+std::vector<cv::Point3i> getMeshVectors(std::vector<cv::Point3f> points, std::vector<cv::Point3f> normals);
 
 int main()
 {
@@ -39,8 +41,12 @@ int main()
 	//法線を求める
 	std::vector<cv::Point3f> normalVecs = getNormalVectors(sampledPoints);
 
+	//メッシュを求める
+	std::vector<cv::Point3i> meshes = getMeshVectors(sampledPoints, normalVecs);
+
 	//PLY形式で保存
-	savePLY_with_normal(sampledPoints, normalVecs, "reconstructPoints_camera.ply");
+	savePLY_with_normal_mesh(sampledPoints, normalVecs, meshes, "reconstructPoints_camera_mesh.ply");
+	//savePLY_with_normal(sampledPoints, normalVecs, "reconstructPoints_camera.ply");
 	//savePLY(sampledPoints, "reconstructPoints_camera_down.ply");
 
 	return 0;
@@ -56,6 +62,41 @@ std::vector<cv::Point3f> loadXMLfile(const std::string &fileName)
 	cvfs["points"] >> reconstructPoints;
 
 	return reconstructPoints;
+}
+
+
+//---保存関係---//
+
+//PLY形式で保存(法線なし)
+void savePLY(std::vector<cv::Point3f> points, const std::string &fileName)
+{
+	//重心を求める
+	double cx = 0, cy = 0, cz = 0;
+	for (int n = 0; n < points.size(); n++){
+		cx += points[n].x;
+		cy += points[n].y;
+		cz += points[n].z;
+	}
+	cx /= points.size();
+	cy /= points.size();
+	cz /= points.size();
+
+	//ファイルオープン
+	FILE *fp;
+	fp = fopen(fileName.data(), "w");
+
+	//ファイルに書き込む
+	//ヘッダの設定
+	fprintf(fp,"ply\nformat ascii 1.0\nelement vertex %d\nproperty float x\nproperty float y\nproperty float z\nend_header\n", points.size());
+
+	//3次元点群
+	//m単位で保存（xmlはmm）
+	//重心を原点にする
+	for (int n = 0; n < points.size(); n++){
+	   fprintf(fp, "%f %f %f \n", (points[n].x - cx), (points[n].y - cy), (points[n].z - cz));
+	}
+	//ファイルクローズ
+	fclose(fp);
 }
 
 //PLY形式で保存(法線あり)
@@ -90,8 +131,8 @@ void savePLY_with_normal(std::vector<cv::Point3f> points, std::vector<cv::Point3
 	fclose(fp);
 }
 
-//PLY形式で保存(法線なし)
-void savePLY(std::vector<cv::Point3f> points, const std::string &fileName)
+//PLY形式で保存(法線あり,meshあり)
+void savePLY_with_normal_mesh(std::vector<cv::Point3f> points, std::vector<cv::Point3f> normals, std::vector<cv::Point3i> meshes, const std::string &fileName)
 {
 	//重心を求める
 	double cx = 0, cy = 0, cz = 0;
@@ -110,17 +151,24 @@ void savePLY(std::vector<cv::Point3f> points, const std::string &fileName)
 
 	//ファイルに書き込む
 	//ヘッダの設定
-	fprintf(fp,"ply\nformat ascii 1.0\nelement vertex %d\nproperty float x\nproperty float y\nproperty float z\nend_header\n", points.size());
+	fprintf(fp,"ply\nformat ascii 1.0\nelement vertex %d\nproperty float x\nproperty float y\nproperty float z\nproperty float nx\nproperty float ny\nproperty float nz\nelement face %d\nproperty list uchar int vertex_indices\nend_header\n", points.size(), meshes.size());
 
 	//3次元点群
 	//m単位で保存（xmlはmm）
 	//重心を原点にする
 	for (int n = 0; n < points.size(); n++){
-	   fprintf(fp, "%f %f %f \n", (points[n].x - cx), (points[n].y - cy), (points[n].z - cz));
+	   fprintf(fp, "%f %f %f %f %f %f \n", (points[n].x - cx), (points[n].y - cy), (points[n].z - cz), normals[n].x, normals[n].y, normals[n].z);
+	}
+	//面情報記述
+	for(int n = 0; n < meshes.size(); n++)
+	{
+	   fprintf(fp, "3 %d %d %d\n", meshes[n].x, meshes[n].y, meshes[n].z);
 	}
 	//ファイルクローズ
 	fclose(fp);
 }
+
+//---Filter関係---//
 
 //法線ベクトルを求める
 std::vector<cv::Point3f> getNormalVectors(std::vector<cv::Point3f> points)
@@ -182,4 +230,53 @@ std::vector<cv::Point3f> getDownSampledPoints(std::vector<cv::Point3f> points, f
 
 	return dst_points;
 }
+
+
+//三角メッシュを生成、メッシュ情報を返す
+std::vector<cv::Point3i> getMeshVectors(std::vector<cv::Point3f> points, std::vector<cv::Point3f> normals)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+	for (int n = 0; n < points.size(); n++){
+		cloud->push_back(pcl::PointXYZ(points[n].x, points[n].y, points[n].z));//単位はm
+		cloud_normals->push_back(pcl::Normal(normals[n].x, normals[n].y, normals[n].z));
+	}
+	// Concatenate the XYZ and normal fields*
+	pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+	pcl::concatenateFields (*cloud, *cloud_normals, *cloud_with_normals);
+
+	// Create search tree*
+	pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+	tree2->setInputCloud (cloud_with_normals);
+
+	// Initialize objects
+	pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+	pcl::PolygonMesh triangles;
+
+	// Set the maximum distance between connected points (maximum edge length)
+	gp3.setSearchRadius (0.025);
+
+	// Set typical values for the parameters
+	gp3.setMu (2.5);
+	gp3.setMaximumNearestNeighbors (100);
+	gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+	gp3.setMinimumAngle(M_PI/18); // 10 degrees
+	gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
+	gp3.setNormalConsistency(false);
+
+	// Get result
+	gp3.setInputCloud (cloud_with_normals);
+	gp3.setSearchMethod (tree2);
+	gp3.reconstruct (triangles);
+
+	//メッシュ情報を返す
+	std::vector<cv::Point3i> dst_meshes;
+	for(int n = 0; n < triangles.polygons.size(); n++)
+	{
+		dst_meshes.emplace_back(cv::Point3i(triangles.polygons[n].vertices[0], triangles.polygons[n].vertices[1], triangles.polygons[n].vertices[2])); 
+	}
+
+	return dst_meshes;
+}
+
 
